d98abdd949f4a58917ded7eb2216adffc18ef55d
sealday <sealday@gmail.com>
2015-08-05 07:50:23 +0800

    remove unused code from app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
 
-// uncomment after placing your favicon in /client
-//app.use(favicon(__dirname + '/client/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,7 +25,7 @@ app.use(function(req, res, next) {
 // error handlers
 //
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.json({
       message: err.message,
