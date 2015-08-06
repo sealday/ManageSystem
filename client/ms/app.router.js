21043336748e503763c773d02feae6f10ad0b478
sealday <sealday@gmail.com>
2015-08-06 10:01:05 +0800

    实验性增加 职工

--- client/ms/app.router.js
+++ client/ms/app.router.js
@@ -18,12 +18,17 @@
         url: '/',
         templateUrl: 'ms/home/home.html',
         controller: 'HomeController',
-        controllerAs: 'hc',
+        controllerAs: 'hc'
       }).state('login', {
         url: '/login',
         templateUrl: 'ms/login/login.html',
         controller: 'LoginController',
         controllerAs: 'lc'
+      }).state('staff', {
+        url: '/staff',
+        templateUrl: 'ms/staff/staff.html',
+        controller: 'StaffController',
+        controllerAs: 'sc'
       });
   }
 })();
---
+++ client/ms/staff/staff.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ms')
+    .controller('StaffController', StaffController);
+
+  function StaffController() {
+    var vm = this;
+
+  }
+})();
